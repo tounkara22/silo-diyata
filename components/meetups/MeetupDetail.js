5dd571f47b6b0195b91d529cfcaa0c2249d6f335
tounkara22 <tounkara@stanford.edu>
2022-03-17 16:51:18 -0400

    Add the module.css file with fix

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,14 +1,14 @@
-import classes from "./meetupDetail.module.css";
+import classes from "./MeetupDetail.module.css";
 
 const MeetupDetail = (props) => {
-    return (
-      <section className={classes.details}>
-        <img src={props.image} alt="Default image"/>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <address>{props.address}</address>
-      </section>
-    );
-}
+  return (
+    <section className={classes.details}>
+      <img src={props.image} alt="Default image" />
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <address>{props.address}</address>
+    </section>
+  );
+};
 
 export default MeetupDetail;
