aec13e8eea052e52c5eed7b7983a091c2a0c1338
tounkara22 <tounkara@stanford.edu>
2022-03-18 03:21:52 -0400

    Add logo and start i18n

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,17 +1,29 @@
 import classes from "./MainNavigation.module.css";
 import Link from "next/link";
+import I18N from "../../i18n/fr";
+import Image from "next/image";
 
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>SiloDiyata</div>
+      <Link href="/">
+        <Image
+          className={classes.logo}
+          src="/logo-bag.png"
+          alt="Silo Diyata logo"
+          width="220"
+          height="50"
+        />
+      </Link>
+
+      {/* <div className={classes.logo}>SiloDiyata</div> */}
       <nav>
         <ul>
           <li>
-            <Link href="/">All Meetups</Link>
+            <Link href="/">{I18N.layout.header.book}</Link>
           </li>
           <li>
-            <Link href="/new-meetup">New Meetup</Link>
+            <Link href="/new-meetup">{I18N.layout.header.contact}</Link>
           </li>
         </ul>
       </nav>
